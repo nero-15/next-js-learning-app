e23e6fcb40d15d63de2cb16e00cee480b3d32fed
nero-15 <nero15.dev@gmail.com>
2021-07-12 22:03:43 +0900

    edit

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'nero-15'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/toka1.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
